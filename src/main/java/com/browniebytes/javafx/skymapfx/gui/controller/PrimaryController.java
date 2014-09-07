e6b3780daaa7dd585ca7da8249e9da9715e44156
taipeiben <taipeiben@hotmail.com>
2014-09-07 15:13:50 -0700

    Adding timer thread to start and stop time update
    Added code to start and stop time update.  Also, adding fields in
    information panel.

--- src/main/java/com/browniebytes/javafx/skymapfx/gui/controller/PrimaryController.java
+++ src/main/java/com/browniebytes/javafx/skymapfx/gui/controller/PrimaryController.java
@@ -2,7 +2,10 @@ package com.browniebytes.javafx.skymapfx.gui.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -34,16 +37,26 @@ public class PrimaryController implements Initializable {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(PrimaryController.class);
 
+	// FXML dependencies
+	@FXML private TextField latitudeTextField;
+	@FXML private TextField longitudeTextField;
+	@FXML private CheckBox useCurrentTimeCheckBox;
+	@FXML private Label mapLocalTimeLabel;
+	@FXML private Label gmtTimeTextField;
+	@FXML private Label jdLabel;
+	@FXML private Label mjdLabel;
+	@FXML private Label gmstLabel;
+	@FXML private Label gastLabel;
+	@FXML private Label lmstLabel;
+
 	// Guice injected dependencies
 	private final ApplicationSettings applicationSettings;
 	private final CatalogFileReader catalogReader;
 	private final SessionFactory sessionFactory;
 	private final ScheduledExecutorService executor;
 
-	@FXML private TextField latitudeTextField;
-	@FXML private TextField longitudeTextField;
-	@FXML private CheckBox useCurrentTimeCheckBox;
-	@FXML private Label mapLocalTimeTextField;
+	// Private instance variables
+	private ScheduledFuture<Void> timeUpdateFuture;
 
 	@Inject
 	public PrimaryController(
@@ -62,20 +75,23 @@ public class PrimaryController implements Initializable {
 	 * Invoked after all @FXML dependencies are injected.  This is run on the application thread.
 	 */
 	@Override
-	public void initialize(URL location, ResourceBundle resources) {
+	public void initialize(final URL location, final ResourceBundle resources) {
 		latitudeTextField.setText(applicationSettings.getSettingAsString(Settings.LATITUDE));
 		longitudeTextField.setText(applicationSettings.getSettingAsString(Settings.LONGITUDE));
+		useCurrentTimeCheckBox.selectedProperty().set(
+				applicationSettings.getSettingAsBoolean(Settings.USE_CURRENT_TIME));
 
 		LOGGER.debug("Starting data initialization");
 		final DataInitializationTask readerTask = new DataInitializationTask();
 		executor.submit(readerTask);
 
-		useCurrentTimeCheckBox.selectedProperty().set(
-				applicationSettings.getSettingAsBoolean(Settings.USE_CURRENT_TIME));
-
 		useCurrentTimeCheckBox.selectedProperty().addListener(
 				(observable, oldValue, newValue) -> {
-					
+					if (newValue) {
+						startTimer();
+					} else {
+						stopTimer();
+					}
 				});
 	}
 
@@ -95,37 +111,57 @@ public class PrimaryController implements Initializable {
 	}
 
 	/**
-	 * This task class checks to see if star data is loaded into the database yet.  If data has not been
-	 * loaded, then it calls the catalog data reader to read data from the data file into the database.
+	 * Starts the timer
+	 */
+	private void startTimer() {
+		LOGGER.debug("Starting time update thread");
+		timeUpdateFuture = (ScheduledFuture<Void>) executor.schedule(
+				new Callable<Void>() {
+					@Override
+					public Void call() throws Exception {
+						return null;
+					}
+				},
+				1,
+				TimeUnit.SECONDS);
+	}
+
+	/**
+	 * Stops the timer
+	 */
+	private void stopTimer() {
+		LOGGER.debug("Stopping time update thread");
+		timeUpdateFuture.cancel(false);
+	}
+
+	/**
+	 * This task class calls the catalog data reader to read data from the data file into the database.
 	 */
 	private class DataInitializationTask extends Task<Void> {
+		/**
+		 * Uses catalog reader to build star database from a data file
+		 */
 		@Override
 		protected Void call() throws Exception {
-			final long starCount = getStarCount();
-			if (starCount == 0) {
-				try {
-					catalogReader.buildStarDatabase();
-					LOGGER.debug("Stars found in database: " + getStarCount());
-				} catch (FatalRuntimeException ex) {
-					// TODO: handle exception
-				}
+			catalogReader.buildStarDatabase();
+
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("Stars found in database: " + getStarCount());
 			}
 			return null;
 		}
 
+		/**
+		 * Executed when data load has completed
+		 */
 		@Override
 		protected void done() {
-			
+			LOGGER.debug("Star database initialization complete");
+
+			if (useCurrentTimeCheckBox.isSelected()) {
+				startTimer();
+			}
 		}
 	}
 
-	/**
-	 * 
-	 */
-	private class TimeUpdateThread implements Runnable {
-		@Override
-		public void run() {
-			
-		}
-	}
 }
