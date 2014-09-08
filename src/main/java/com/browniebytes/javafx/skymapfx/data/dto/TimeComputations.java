094fbf86b63493d481f5b0ad0a2843ed9291fc8f
taipeiben <taipeiben@hotmail.com>
2014-09-08 10:00:12 -0700

    Adding DateTimePicker
    Adding DateTimePicker control to project

---
+++ src/main/java/com/browniebytes/javafx/skymapfx/data/dto/TimeComputations.java
@@ -0,0 +1,137 @@
+package com.browniebytes.javafx.skymapfx.data.dto;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class TimeComputations {
+
+	private static final ZoneId GMT_ZONE = ZoneId.of("GMT");
+
+	private final LocalDateTime localDateTime;
+	private final double longitude;
+	private final ZonedDateTime gmtTime;
+	private final double jd;
+	private final double mjd;
+	private final double gmst;
+	private final double gast;
+	private final double lmst;
+
+	public TimeComputations(final LocalDateTime localDateTime, final double longitude) {
+		this.localDateTime = localDateTime;
+		this.longitude = longitude;
+
+		// Compute GMT Time
+		final ZoneId localZone = ZoneId.systemDefault();
+		final ZonedDateTime zonedLocalTime = ZonedDateTime.of(localDateTime, localZone);
+		this.gmtTime = zonedLocalTime.withZoneSameInstant(GMT_ZONE);
+
+		// Compute Julian Day
+		this.jd = convertGregorianDateTimeToJulianDay(
+				gmtTime.getYear(),
+				gmtTime.getMonth().getValue(),
+				gmtTime.getDayOfMonth(),
+				gmtTime.getHour(),
+				gmtTime.getMinute(),
+				gmtTime.getSecond());
+
+		// Compute Modified Julian Day
+		this.mjd = convertJulianDayToModifiedJulianDay();
+
+		// Compute GMST
+		this.gmst = convertJulianDayToGMST();
+
+		// Compute GAST
+		this.gast = convertJulianDayToGAST();
+
+		// Compute LMST
+		this.lmst = convertJulianDayToLMST();
+	}
+
+	public LocalDateTime getLocalDateTime() {
+		return localDateTime;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public ZonedDateTime getGmtTime() {
+		return gmtTime;
+	}
+
+	public double getJd() {
+		return jd;
+	}
+
+	public double getMjd() {
+		return mjd;
+	}
+
+	public double getGmst() {
+		return gmst;
+	}
+
+	public double getGast() {
+		return gast;
+	}
+
+	public double getLmst() {
+		return lmst;
+	}
+
+	private double convertGregorianDateTimeToJulianDay(
+			final double year,
+			final double month,
+			final double day,
+			final double hour,
+			final double minute,
+			final double second) {
+
+		final double a = Math.floor((14 - month) / 12.0);
+		final double y = year + 4800.0 - a;
+		final double m = month + 12*a - 3;
+
+		final double jdn =
+				day + Math.floor((153*m+2)/5) + 365.0*y + Math.floor(y/4)-Math.floor(y/100)+Math.floor(y/400)-32045.0;
+		final double jd =
+				jdn + (hour - 12.0)/24.0 + minute/1440.0 + second/86400.0;
+
+		return jd;
+	}
+
+	private double convertJulianDayToModifiedJulianDay() {
+		return jd - 2400000.5;
+	}
+
+	private double convertJulianDayToGMST() {
+		final double d = jd - 2451545.0;
+		final double T = d/36525;
+
+		double sidereal = 280.46061837 + (360.98564736629 * (jd - 2451545.0)) + (0.000387933 * T * T) - (T * T * T / 38710000.0);
+		sidereal %= 360;
+		if (sidereal < 0) {
+			sidereal += 360;
+		}
+		return sidereal;
+	}
+
+	private double convertJulianDayToGAST() {
+
+		final double d = jd - 2451545.0;
+		final double omega = 125.04 - 0.052954 * d;
+		final double l = 280.47 + 0.98565 * d;
+		final double e = 23.4393 - 0.0000004 * d;
+
+		final double w = -0.000319 * Math.sin(omega * Math.PI/180) - 0.000024 * Math.sin((2 * l) * Math.PI/180);
+		final double correction = w * Math.cos(e * Math.PI/180);
+
+		return gmst + correction * 15;
+	}
+
+	private double convertJulianDayToLMST() {
+		final double lmst = gmst + longitude;
+		return lmst;
+	}
+
+}
--- src/main/java/com/browniebytes/javafx/skymapfx/gui/controller/PrimaryController.java
+++ src/main/java/com/browniebytes/javafx/skymapfx/gui/controller/PrimaryController.java
@@ -1,12 +1,15 @@
 package com.browniebytes.javafx.skymapfx.gui.controller;
 
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
-import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
+import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -21,8 +24,10 @@ import org.hibernate.criterion.Projections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.browniebytes.javafx.control.DateTimePicker;
 import com.browniebytes.javafx.skymapfx.ApplicationSettings;
 import com.browniebytes.javafx.skymapfx.ApplicationSettings.Settings;
+import com.browniebytes.javafx.skymapfx.data.dto.TimeComputations;
 import com.browniebytes.javafx.skymapfx.data.entities.Star;
 import com.browniebytes.javafx.skymapfx.data.io.CatalogFileReader;
 import com.google.inject.Inject;
@@ -33,26 +38,39 @@ import com.google.inject.Inject;
 public class PrimaryController implements Initializable {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(PrimaryController.class);
+	private static final DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern("MM/d/yy HH:mm:ss a");
+	private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
 
+	// ========================================
 	// FXML dependencies
+	// ========================================
+
+	// User Context
 	@FXML private TextField latitudeTextField;
 	@FXML private TextField longitudeTextField;
 	@FXML private CheckBox useCurrentTimeCheckBox;
+	@FXML private DateTimePicker dateTimePicker;
+
+	// Time Information
 	@FXML private Label mapLocalTimeLabel;
-	@FXML private Label gmtTimeTextField;
+	@FXML private Label gmtTimeLabel;
 	@FXML private Label jdLabel;
 	@FXML private Label mjdLabel;
 	@FXML private Label gmstLabel;
 	@FXML private Label gastLabel;
 	@FXML private Label lmstLabel;
 
+	// ========================================
 	// Guice injected dependencies
+	// ========================================
 	private final ApplicationSettings applicationSettings;
 	private final CatalogFileReader catalogReader;
 	private final SessionFactory sessionFactory;
 	private final ScheduledExecutorService executor;
 
+	// ========================================
 	// Private instance variables
+	// ========================================
 	private ScheduledFuture<Void> timeUpdateFuture;
 
 	@Inject
@@ -110,17 +128,28 @@ public class PrimaryController implements Initializable {
 	/**
 	 * Starts the timer
 	 */
+	@SuppressWarnings("unchecked")
 	private void startTimer() {
 		LOGGER.debug("Starting time update thread");
-		timeUpdateFuture = (ScheduledFuture<Void>) executor.schedule(
-				new Callable<Void>() {
+		timeUpdateFuture = (ScheduledFuture<Void>) executor.scheduleAtFixedRate(
+				new Runnable() {
 					@Override
-					public Void call() throws Exception {
-						return null;
+					public void run() {
+						final LocalDateTime localDateTime = LocalDateTime.now();
+
+						LOGGER.debug("Setting date/time to: " + localDateTime.toString());
+
+						Platform.runLater(
+								() -> {
+									// Changing UI value must be done on application thread
+									dateTimePicker.dateTimeProperty().set(localDateTime);
+									startTimeComputations();
+								});
 					}
 				},
-				1,
-				TimeUnit.SECONDS);
+				0L,  // no delay
+				10L, // period
+				TimeUnit.SECONDS); // in seconds
 	}
 
 	/**
@@ -129,6 +158,13 @@ public class PrimaryController implements Initializable {
 	private void stopTimer() {
 		LOGGER.debug("Stopping time update thread");
 		timeUpdateFuture.cancel(false);
+	}
+
+	/**
+	 * Runs time computations on separate thread
+	 */
+	private void startTimeComputations() {
+		executor.submit(new TimeComputationTask());
 	}
 
 	/**
@@ -158,6 +194,44 @@ public class PrimaryController implements Initializable {
 			if (useCurrentTimeCheckBox.isSelected()) {
 				startTimer();
 			}
+		}
+	}
+
+	private class TimeComputationTask extends Task<TimeComputations> {
+		@Override
+		protected TimeComputations call() throws Exception {
+			final LocalDateTime localDateTime = dateTimePicker.dateTimeProperty().get();
+
+			final long start = System.currentTimeMillis();
+			final TimeComputations computations = new TimeComputations(
+					localDateTime,
+					Double.parseDouble(longitudeTextField.textProperty().get())); // TODO: process NumberFormatException
+
+			LOGGER.debug(
+					String.format(
+							"Time computations completed in %d ms",
+							System.currentTimeMillis() - start));
+
+			return computations;
+		}
+
+		@Override
+		protected void done() {
+			try {
+				LOGGER.debug("Setting time computation labels ...");
+
+				final TimeComputations computations = get();
+				mapLocalTimeLabel.textProperty().set(computations.getLocalDateTime().toString());
+				gmtTimeLabel.setText(computations.getGmtTime().toString());
+				jdLabel.setText(String.format("%.3f", computations.getJd()));
+				mjdLabel.setText(String.format("%.3f", computations.getMjd()));
+				gmstLabel.setText(String.format("%.3f", computations.getGmst()));
+				gastLabel.setText(String.format("%.3f", computations.getGast()));
+				lmstLabel.setText(String.format("%.3f", computations.getLmst()));
+			} catch (ExecutionException ex) {
+				// TODO: handle exception
+				LOGGER.error("Execution exception", ex);
+			} catch (InterruptedException ex) {}  // Do nothing if interrupted
 		}
 	}
 
